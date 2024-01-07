c93df580d5428bdcfdc319c275a16486937140a4
Shubham <shubhamyadav32100@gmail.com>
2024-01-07 18:30:15 +0530

    Implemented various features:

--- server/server.js
+++ server/server.js
@@ -209,7 +209,6 @@ server.post('/create-blog', verifyJWT, (req, res) => {
   let blog = new Blog({
     title, des, banner, content, tags, author: authorId, blog_id, draft: Boolean(draft)
   })
-
   blog.save().then((blog) => {
     let incrementVal = draft ? 0 : 1;
     User.findOneAndUpdate(
@@ -224,6 +223,82 @@ server.post('/create-blog', verifyJWT, (req, res) => {
     return res.status(500).json({ error: err.message })
   })
 })
+
+// Get the latest-blogs
+server.post("/latest-blogs", (req, res) => {
+  let maxLimit = 5;
+  let { page } = req.body;
+  Blog.find({ draft: false })
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+    .sort({ "publishedAt": -1 })
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs })
+    })
+    .catch((error) => {
+      return res.status(500).json({ error: error.message })
+    })
+})
+
+server.post("/all-latest-blogs-count", (req, res) => {
+  Blog.countDocuments({ draft: false })
+    .then((count) => {
+      return res.status(200).json({ totalDocs: count })
+    })
+    .catch((err) => {
+      return res.status(500).json({ err: err.message })
+    })
+})
+
+// Get the trending-blogs
+server.get("/trending-blogs", (req, res) => {
+  let maxLimit = 5;
+  Blog.find({ draft: false })
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+    .sort({ "activity.total_read": -1, "activity.total_likes": -1, "publishedAt": -1 })
+    .select("blog_id title publishedAt -_id")
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs })
+    })
+    .catch((error) => {
+      return res.status(500).json({ error: error.message })
+    })
+})
+
+// SEARCH BLOGS
+server.post("/search-blogs", (req, res) => {
+  let { tag, page } = req.body;
+  let findQuery = { tags: tag, draft: false };
+  let maxLimit = 5;
+  Blog.find(findQuery)
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+    .sort({ publishedAt: -1 })
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((error) => {
+      return res.status(500).json({ error: error.message });
+    });
+});
+
+server.post("/search-blogs-count", (req, res) => {
+  let { tag } = req.body;
+  let findQuery = { tags: tag, draft: false }
+  Blog.countDocuments(findQuery)
+    .then(count => {
+      return res.status(200).json({ totalDocs: count })
+    })
+    .catch(err => {
+      return res.send(500).json({ err: err.message })
+    })
+})
+
 
 // Creating Server
 server.listen(PORT, () => {
